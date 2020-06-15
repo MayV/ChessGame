4474b894c097a4ea990df1f7f292f22788275167
Mayur Vaid <mayur26vaid@gmail.com>
2020-06-15 19:55:59 +0530

    Create Move.java

---
+++ entity/Move.java
@@ -0,0 +1,13 @@
+public class Move {
+	private Position src, dest;
+	
+	public Position getSrc() {
+		return src;
+	}
+
+	public Position getDest() {
+		return dest;
+	}
+
+	//Initialize setters.
+}
